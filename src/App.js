992e5553ceda1968a3a8e531d0d637d6cccb4cba
Clifbn <cliben01@easv365.dk>
2026-03-13 00:00:33 +0100

    3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/navBar';
 import { Banner } from './components/banner';
-import { Skills } from './components/Skills'
 import { Projects } from './components/Projects'
 import { Video } from './components/Video'
 import { Contact } from './components/Contact';
